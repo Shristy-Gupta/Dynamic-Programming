a8b299a2f59a97e589297ce7b0dd9f5db0f0384b
Shristy Gupta <shristysgupta@gmail.com>
2021-07-13 15:23:57 +0530

    Create 32 Count number of ways to reach a given score in a game| practice.cpp

---
+++ 450DSACrackerDP/32 Count number of ways to reach a given score in a game| practice.cpp
@@ -0,0 +1,48 @@
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ // } Driver Code Ends
+// Complete this function
+long long int count(long long int n)
+{
+	long long int table[n+1],i;
+	memset(table, 0, sizeof(table));
+	table[0]=1;                 // If 0 sum is required number of ways is 1.
+	
+    for(int i=3;i<=n;i++){
+        if(i>=3){
+            table[i]+=table[i-3];
+        }
+    }
+    for(int i=5;i<=n;i++){
+        if(i>=5){
+            table[i]+=table[i-5];
+        }
+    }
+     for(int i=10;i<=n;i++){
+        if(i>=10){
+            table[i]+=table[i-10];
+        }
+    }
+	
+	return table[n];
+}
+
+// { Driver Code Starts.
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		cout<<count(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
