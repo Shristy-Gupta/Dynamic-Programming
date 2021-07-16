45053ce7b13936bba861333cc4091dd29a7122f3
Shristy Gupta <shristysgupta@gmail.com>
2021-07-17 02:34:40 +0530

    Update 43	Longest alternating subsequence| LIS variant.cpp

--- 450DSACrackerDP/43	Longest alternating subsequence| LIS variant.cpp
+++ 450DSACrackerDP/43	Longest alternating subsequence| LIS variant.cpp
@@ -49,3 +49,24 @@ int main(){
 	}
 	return 0;
 }  // } Driver Code Ends
+
+
+
+///////////////////// optimized
+int AlternatingaMaxLength(vector<int>&nums){
+            int n=nums.size();
+            int ma=1;
+            int mi=1;
+            for(int i=1;i<n;i++){
+                if(nums[i]>nums[i-1]){
+                    ma=mi+1;
+                }
+                else if(nums[i]<nums[i-1]){
+                    mi=ma+1;
+                }
+                else{
+                    continue;
+                }
+            }
+            return max(ma,mi);
+		}
